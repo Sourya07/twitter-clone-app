bdb67f05714ef6d9d0c79ac73d98da347939a310
Sourya07 <singhsourya137@gmail.com>
2024-11-14 20:29:29 +0530

    rewriting the compoments

--- frontend/src/components/Rightsidebar.js
+++ frontend/src/components/Rightsidebar.js
@@ -4,6 +4,8 @@ function Rightsidebar() {
   return (
     <div className='w-[20%]'>
       <div className='bg-white p-2 rounded-lg shadow-md'>Recent News</div>
+
+
       <div>hlo there </div>
 
     </div>
